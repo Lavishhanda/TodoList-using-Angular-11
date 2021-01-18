b17afb10de8eee6da198ea0363cb6dbab31ec27d
Lavish Handa <lavish.handa@gmail.com>
2021-01-17 23:23:43 -0500

    Angular ToDoList Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name:string = 'loda';
+
+  constructor(){
+    this.changeName("Joh")
+  }
+
+  changeName(name:string):void{
+    this.name = name;
+
+  }
 }
